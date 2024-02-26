a3c1b055cdad771e2bdbf82bd8da777b4accc0c3
Dinuk002 <dinukoffcl@gmail.com>
2024-02-26 23:44:41 +0530

    Created the list tiles and check boxes

--- lib/components/todo_tile.dart
+++ lib/components/todo_tile.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
       child: Container(
         padding: EdgeInsets.all(24),
         child: Row(
           children: [
-            Text("Make Tutorial"),
+            //check box
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+            ),
+            //task name
+            Text(
+              taskName,
+              style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
+            ),
           ],
         ),
         decoration: BoxDecoration(
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//List of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Exercise", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +29,15 @@ class _HomePageState extends State<HomePage> {
         title: Center(child: Text("TO DO LIST")),
         backgroundColor: Colors.deepPurple,
       ),
-      body: ListView(
-        children: [ToDoTile()],
-      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return ToDoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+            );
+          }),
     );
   }
 }
